3f0004ceb8fc1706df1261355e985aa79ae3434e
202231685 <202231685@PO-21503>
2023-09-08 14:32:34 -0400

    Étape 1 faite

--- JeuGladiateurs/src/personnages/Personnage.java
+++ JeuGladiateurs/src/personnages/Personnage.java
@@ -6,7 +6,11 @@ public class Personnage {
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Attributs">
-    // TODO : Mettre vos attributs ici
+        String nom;
+        int pointsDeVie;
+        int valeurMaxAttaque;
+        int valeurDefense;
+        int initiative;
     // </editor-fold>
 
     // **************************************************************************
@@ -14,11 +18,19 @@ public class Personnage {
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Constructeurs et initialisation">
     public Personnage(String nom, int attaqueMax, int defense, int pvs, int ini) {
-        // TODO : Constructeur AVEC paramètres
+        this.nom = nom;
+        pointsDeVie = pvs;
+        valeurMaxAttaque = attaqueMax;
+        valeurDefense = defense;
+        initiative = ini;
     }
 
     public Personnage() {
-        // TODO : Constructeur SANS paramètres qui initialise à ZÉRO ou à ""
+        this.nom = "";
+        pointsDeVie = 0;
+        valeurMaxAttaque = 0;
+        valeurDefense = 0;
+        initiative = 0;
     }
     // </editor-fold>
 
@@ -26,10 +38,47 @@ public class Personnage {
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Getters et setters">
-    // TODO : Les getters
-    // TODO : Les setters
-    // </editor-fold>
+    public String getNom() {    
+        return nom;
+        // TODO : Afficher les infos du personnage, tel que montré dans l'énoncé
+    }
 
+    public int getPointsDeVie() {
+        return pointsDeVie;
+    }
+
+    public int getValeurMaxAttaque() {
+        return valeurMaxAttaque;
+    }
+
+    public int getValeurDefense() {
+        return valeurDefense;
+    }
+
+    public int getInitiative() {
+        return initiative;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPointsDeVie(int pointsDeVie) {
+        this.pointsDeVie = pointsDeVie;
+    }
+
+    public void setValeurMaxAttaque(int valeurMaxAttaque) {
+        this.valeurMaxAttaque = valeurMaxAttaque;
+    }
+
+    public void setValeurDefense(int valeurDefense) {
+        this.valeurDefense = valeurDefense;
+    }
+
+    public void setInitiative(int initiative) {
+        this.initiative = initiative;
+    }
+    // </editor-fold>
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
