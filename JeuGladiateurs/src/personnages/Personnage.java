690d60ce51a4be5b75c8dab4c75248408ee15a6d
202231685 <202231685@PO-21503>
2023-09-08 14:44:47 -0400

    Étape 2 faite

--- JeuGladiateurs/src/personnages/Personnage.java
+++ JeuGladiateurs/src/personnages/Personnage.java
@@ -85,6 +85,18 @@ public class Personnage {
     // <editor-fold defaultstate="collapsed" desc="Mécanique de jeu">
     public void afficherInfosPersonnage() {
         // TODO : Afficher les infos du personnage, tel que montré dans l'énoncé
+        System.out.println("");
+        System.out.println(nom);
+        System.out.println("    Attaque : " + valeurMaxAttaque);
+        System.out.println("    Défense : " + valeurDefense);
+        System.out.println("    Points de vie : " + pointsDeVie);
+        System.out.println("    Initiative : " + initiative);
+        if (pointsDeVie <=0) {
+            System.out.println("    Statut : Mort");
+        }
+        else {
+            System.out.println("    Statut : Vivant");
+        }
     }
 
     private int attaqueCalcul() {
