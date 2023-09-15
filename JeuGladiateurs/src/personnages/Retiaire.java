c41053d71c98b029f1ddca7cd8e20db4993a9352
202231685 <202231685@PO-21503>
2023-09-15 14:57:35 -0400

    Recréation de mes classes mirmillon et retiaire et Mise en majuscule de mes noms de classe Mirmillon et Retiaire

---
+++ JeuGladiateurs/src/personnages/Retiaire.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package personnages;
+
+/**
+ *
+ * @author Thomas Garant
+ */
+public class Retiaire {
+    // ****************************************
+    // <editor-fold desc="ATTRIBUTS">
+
+    // </editor-fold>
+
+    //*****************************************
+    //<editor-fold defaultstate="collapsed" desc="CONSTRUCTEURS et INITIALISATION">
+
+    //</editor-fold>
+
+    //*****************************************
+    //<editor-fold desc="MÉTHODES">
+
+    //</editor-fold>
+
+    //*****************************************
+    //<editor-fold defaultstate="collapsed" desc="GETTERS et SETTERS">
+
+    //</editor-fold>
+}
